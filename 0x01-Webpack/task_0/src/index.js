3ab8c67a58019596d7d329386cba162525bde4d1
Victor Zuluaga <zrvictor@outlook.com>
2021-10-29 15:23:43 -0500

    Fix 3 paragraph, 3 bodies

--- 0x01-Webpack/task_0/src/index.js
+++ 0x01-Webpack/task_0/src/index.js
@@ -2,6 +2,10 @@ import $ from "jquery";
 
 $('body').html(`
     <p>Holberton Dashboard<p>
+`);
+$('body').html(`
     <p>Dashboard data for the students<p>
+`);
+$('body').html(`
     <p>Copyright - Holberton School<p>
 `);
