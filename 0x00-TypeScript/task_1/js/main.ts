6393a5cee960f608728b2bf7b2a653c7171c6ea1
Victor Zuluaga <zrvictor@outlook.com>
2021-10-30 14:30:58 -0500

    extend functionallity

--- 0x00-TypeScript/task_1/js/main.ts
+++ 0x00-TypeScript/task_1/js/main.ts
@@ -15,3 +15,47 @@ const teacher3: Teacher = {
     contract: false,
 };
 console.log(teacher3);
+// 2. Extending the Teacher class
+interface Directors extends Teacher {
+numberOfReports: number,
+};
+    const director1: Directors = {
+    firstName: 'John',
+    lastName: 'Doe',
+    location: 'London',
+    fullTimeEmployee: true,
+    numberOfReports: 17,
+};
+//console.log(director1);
+
+// 3. Printing teachers
+interface printTeacherFunction {
+    (firstName: string, lastName: string): string;
+}
+
+export const printTeacher: printTeacherFunction = (firstName: string, lastName: string): string =>`${firstName.charAt(0)}. ${lastName}`;
+
+//console.log(printTeacher("John", "Doe"));
+
+// 4. Writing a class
+interface StudentClassConstructor {
+    new(firstName: string, lastName: string): StudentClassInterface;
+}
+interface StudentClassInterface {
+    firstName: string;
+    lastName: string;
+}
+class StudentClass implements StudentClassInterface {
+    firstName: string;
+    lastName: string;
+    constructor(firstName: string, lastName: string) {
+      this.firstName = firstName;
+      this.lastName = lastName;
+    }
+    workOnHomework() {
+      return 'Currently working';
+    }
+    displayName() {
+      return this.firstName;
+    }
+}
